d05fa8ec6092f12488846efb487315ea73690028
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-30 15:11:41 -0500

    @jaredstevick @johnalxndr things

--- js/main.js
+++ js/main.js
@@ -1,37 +1,17 @@
-var _ = require('lodash'); 
-var board = [
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-];
+function print(){
+    document.write('<table border="1">');
+    
+    
+    _.times(8, function(){
+        document.write('<tr>');
+        _.times(8, function(){
+            document.write('<td></td>');
+        });
+        document.write('</tr>');
+    });
+    
+    
+    document.write('</table>');
+}
 
-function print(board){
-  // Before any rows are printed, print the `<table>`...
-  console.log('<table class="chessboard">');
-
-  _.forEach(board, function(row, rank){
-      // Before any cells are printed...
-      console.log({
-          'row': "<tr>" // print the table row
-      });   
-      _.forEach(row, function(square, file){
-            console.log({
-                'file': '<td></td>', // td for each square
-                'square': square // shows blank space or letter for piece
-            })
-      }); // END foreach(row)
-    console.log({
-          'row': "</tr>"
-      });
-      // After all the cells are printed...
-  }); // END foreach(board)
-  // After all the rows are printed...
-  console.log('</table> <!-- .chessboard -->');
-} 
-
-print(board);
+print();
