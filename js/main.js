cb90b6015ac0f387e66b601411b4be3b990255a5
Jon Rutishauser <jrutishauser@gmail.com>
2015-01-30 12:33:40 -0500

    added some comments about what each key/value request is doing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 var _ = require('lodash'); 
-
-
 var board = [
     ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
@@ -12,33 +10,26 @@ var board = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
 ];
 
-
 function print(board){
   // Before any rows are printed, print the `<table>`...
   console.log('<table class="chessboard">');
 
   _.forEach(board, function(row, rank){
-
       // Before any cells are printed...
-
       console.log({
-          'rank': "<tr>"
-      });
-      
+          'row': "<tr>" // print the table row
+      });   
       _.forEach(row, function(square, file){
-
             console.log({
-                'file': "<td></td>",
-                'square': square,
+                'file': '<td></td>', // td for each square
+                'square': square // shows blank space or letter for piece
             })
       }); // END foreach(row)
     console.log({
-          'rank': "</tr>"
+          'row': "</tr>"
       });
       // After all the cells are printed...
-
   }); // END foreach(board)
-
   // After all the rows are printed...
   console.log('</table> <!-- .chessboard -->');
 } 
