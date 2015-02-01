78caa12978d23a2f0e276322cf217ba343ecd123
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-01 18:19:18 -0500

    attempt at a refactor from 0 or maybe just version 2.0 who knows

--- js/main.js
+++ js/main.js
@@ -1,182 +1,77 @@
-function print(){
+var board = [];
 
-    var rowIdentifier = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-    document.write('<table>');
-
-    var index = 0;
-
-    _.forEach(board, function(row, key){
-
-        document.write("<tr>");
-        var idNumber = 1;
-
-        _.forEach(row, function(square, k){
-
-            var squareId = rowIdentifier[index] + idNumber;
-
-            document.write("<td id=" + squareId + ">" + square + "</td>")
-            idNumber++;
-
-        });
-
-        index++;
-        document.write("</tr>");
-    });
-
-    document.write('</table>');
-}
+//@@@@@@ this board builder is what dave gave us. @@@@
+// @@@@@@ keep in mind he wanted us to build the board from our javascript array@@@@@
 
 
+function print(board){
+  // Before any rows are printed, print the `<table>`...
+  console.log('<table class="chessboard">');
 
-//function print(board){
-//    document.write('<div class="container">'); // need something better this rewures everything
-//    document.write('<table class="chessboard">'); // need something better this rewures everything
-//
-//    _.forEach(board, function(row, key){ // need something better this rewures everything
-//        document.write('<tr>'); // need something better this rewures everything
-//        _.forEach(row, function(square, k){ // need something better this rewures everything
-//            document.write('<td>' + square + '</td>'); // need something better this rewures everything
-//
-//        });
-//        document.write('</tr>'); // need something better this rewures everything
-//    });
-//
-//    document.write('</table>');
-//        document.write('</div>');
-//}
+  _.forEach(board, function(row, rank){
 
-var board = {};
-board.a = {
-    'one': '<span class="piece black rook"></span>',
-    'two': '<span class="piece black knight"></span>',
-    'three': '<span class="piece black bishop"></span>',
-    'four': '<span class="piece black queen"></span>',
-    'five': '<span class="piece black king"></span>',
-    'six': '<span class="piece black bishop"></span>',
-    'seven': '<span class="piece black knight"></span>',
-    'eight': '<span class="piece black rook"></span>'
+      // Before any cells are printed...
+
+      _.forEach(row, function(square, file){
+
+          // In here, we have `rank`, `file`, and `square`...
+
+      }); // END foreach(row)
+
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
+
+
+
+// will house references to all pieces
+var pieces = [];
+
+// piece constructor. when used like new piece() it will 
+// make a new instance of a piece
+var piece = function(type, color, x, y) {
+    this.type = type;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.assignCell = function(x, y) {
+        board[x][y] = this;
+    };
 };
-board.b = {
-    'one': '<span class="piece black pawn"></span>',
-    'two': '<span class="piece black pawn"></span>',
-    'three': '<span class="piece black pawn"></span>',
-    'four': '<span class="piece black pawn"></span>',
-    'five': '<span class="piece black pawn"></span>',
-    'six': '<span class="piece black pawn"></span>',
-    'seven': '<span class="piece black pawn"></span>',
-    'eight': '<span class="piece black pawn"></span>'
-};
-board.c = {
-    'one': '',
-    'two': '',
-    'three': '',
-    'four': '',
-    'five': '',
-    'six': '',
-    'seven': '',
-    'eight': ''
-};
-board.d = {
-    'one': '',
-    'two': '',
-    'three': '',
-    'four': '',
-    'five': '',
-    'six': '',
-    'seven': '',
-    'eight': ''
-};
-board.e = {
-    'one': '',
-    'two': '',
-    'three': '',
-    'four': '',
-    'five': '',
-    'six': '',
-    'seven': '',
-    'eight': ''
-};
-board.f = {
-    'one': '',
-    'two': '',
-    'three': '',
-    'four': '',
-    'five': '',
-    'six': '',
-    'seven': '',
-    'eight': ''
-};
-board.g = {
-    'one': '<span class="piece white pawn"></span>',
-    'two': '<span class="piece white pawn"></span>',
-    'three': '<span class="piece white pawn"></span>',
-    'four': '<span class="piece white pawn"></span>',
-    'five': '<span class="piece white pawn"></span>',
-    'six': '<span class="piece white pawn"></span>',
-    'seven': '<span class="piece white pawn"></span>',
-    'eight': '<span class="piece white pawn"></span>'
-};
-board.h = {
-    'one': '<span class="piece white rook"></span>',
-    'two': '<span class="piece white knight"></span>',
-    'three': '<span class="piece white bishop"></span>',
-    'four': '<span class="piece white queen"></span>',
-    'five': '<span class="piece white king"></span>',
-    'six': '<span class="piece white bishop"></span>',
-    'seven': '<span class="piece white knight"></span>',
-    'eight': '<span class="piece white rook"></span>'
-};
-print(board);
 
 
-var whichMove = 0;
+var generatePieces = function() {
+    // have 8 * 8 loop make all 32 pieces
+    
+    // rooks
+    pieces.push(new piece('rook', 'white', 0, 0));
+    pieces.push(new piece('rook', 'white', 7, 0));
+    pieces.push(new piece('rook', 'black', 0, 7));
+    pieces.push(new piece('rook', 'black', 7, 7));
 
-function movePiece(){
+    pieces.push(new piece('knight', 'white', 1, 0));
+    pieces.push(new piece('knight', 'white', 6, 0));
+    pieces.push(new piece('knight', 'black', 7, 6));
+    pieces.push(new piece('knight', 'black', 7, 1));
 
-if (whichMove == 0) {
-    var lContent = document.getElementById('g4').innerHTML;
-    document.getElementById('g4').innerHTML = "";
-    document.getElementById('e4').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 1) {
-    var lContent = document.getElementById('a7').innerHTML;
-    document.getElementById('a7').innerHTML = "",
-    document.getElementById('c6').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 2){
-    var lContent = document.getElementById('g3').innerHTML;
-    document.getElementById('g3').innerHTML = "",
-    document.getElementById('e3').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 3){
-    var lContent = document.getElementById('b5').innerHTML;
-    document.getElementById('b5').innerHTML = "",
-    document.getElementById('c5').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 4){
-    var lContent = document.getElementById('g7').innerHTML;
-    document.getElementById('g7').innerHTML = "",
-    document.getElementById('f7').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 5){
-    var lContent = document.getElementById('b4').innerHTML;
-    document.getElementById('b4').innerHTML = "",
-    document.getElementById('d4').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 6){
-    var lContent = document.getElementById('h6').innerHTML;
-    document.getElementById('h6').innerHTML = "",
-    document.getElementById('g7').innerHTML = lContent;
-    whichMove++;
-} else if (whichMove == 7){
-    var lContent = document.getElementById('a6').innerHTML;
-    document.getElementById('a6').innerHTML = "",
-    document.getElementById('b5').innerHTML = lContent;
-    whichMove++;
-} else {
-    var lContent = document.getElementById('h7').innerHTML;
-    document.getElementById('h7').innerHTML = "",
-    document.getElementById('f6').innerHTML = lContent;
-    }
+    // do this for the rest of the pieces
+    pieces.push(new piece('bishop', 'white', 0, 0));
+    pieces.push(new piece('bishop', 'white', 7, 0));
+    pieces.push(new piece('bishop', 'black', 0, 7));
+    pieces.push(new piece('bishop', 'black', 7, 7));
+
+};
+
+var generateBoard = function() {
+    // have 8 * 8 make array "2-dimensional matrix"
+    // during this phase you will simply make a large matrix full of board references
+    // alternatively do
+    peices[0].assignCell(0,0);
+    
+    board[0] = [];
+    board[0][0] = peices[0];
 };
